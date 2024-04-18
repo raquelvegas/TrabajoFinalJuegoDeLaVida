f4675bc2016abd849475b442e0fa1d17c771a3b6
raquelvegas <raquel.vegas@edu.uah.es>
2024-04-18 10:26:54 +0200

    18-04 10.26

--- src/main/java/com/example/NewInterfaz/ControllerInterfazBase.java
+++ src/main/java/com/example/NewInterfaz/ControllerInterfazBase.java
@@ -4,6 +4,8 @@ package com.example.NewInterfaz;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
+import javafx.scene.shape.Circle;
 
 public class ControllerInterfazBase {
 
@@ -12,23 +14,48 @@ public class ControllerInterfazBase {
     @FXML
     private ImageView imageViewPackman;
     @FXML
+    private ImageView imageViewCuadroTexto;
+    @FXML
     private ImageView imageViewLogo;
     @FXML
     private ImageView imageViewVivos;
     @FXML
     private ImageView imageViewMuertos;
+    @FXML
+    private ImageView buttonViewPause;
+    @FXML
+    private ImageView buttonViewPlay;
+    @FXML
+    private ImageView buttonViewSonido;
+    @FXML
+    private ImageView buttonviewStop;
+    @FXML
+    private ImageView buttonViewVelocidad;
 
     public void initialize() {
-        Image imageLogo = new Image("file:///C:/Users/Raquel/OneDrive%20-%20Universidad%20de%20Alcala/Escritorio/TrabajoFinal/TrabajoFinalJuegoDeLaVida/src/main/java/com/example/NewInterfaz/Icon3_preview_rev_1.png");
+        Image imageLogo = new Image("file:///C:/Users/Raquel/OneDrive%20-%20Universidad%20de%20Alcala/Escritorio/TrabajoFinal/TrabajoFinalJuegoDeLaVida/src/main/java/com/example/NewInterfaz/Images/IconLifeGame.png");
         imageViewLogo.setImage(imageLogo);
-        Image imagePacman = new Image("file:///C:/Users/Raquel/OneDrive%20-%20Universidad%20de%20Alcala/Escritorio/TrabajoFinal/TrabajoFinalJuegoDeLaVida/src/main/java/com/example/NewInterfaz/pacman.png");
+        Image imagePacman = new Image("file:///C:/Users/Raquel/OneDrive%20-%20Universidad%20de%20Alcala/Escritorio/TrabajoFinal/TrabajoFinalJuegoDeLaVida/src/main/java/com/example/NewInterfaz/Images/pacman.png");
         imageViewPackman.setImage(imagePacman);
-        Image imageVivos = new Image("file:///C:/Users/Raquel/OneDrive%20-%20Universidad%20de%20Alcala/Escritorio/TrabajoFinal/TrabajoFinalJuegoDeLaVida/src/main/java/com/example/NewInterfaz/vivos.png");
+        Image imageVivos = new Image("file:///C:/Users/Raquel/OneDrive%20-%20Universidad%20de%20Alcala/Escritorio/TrabajoFinal/TrabajoFinalJuegoDeLaVida/src/main/java/com/example/NewInterfaz/Images/vivos.png");
         imageViewVivos.setImage(imageVivos);
-        Image imageMuertos = new Image("file:///C:/Users/Raquel/OneDrive%20-%20Universidad%20de%20Alcala/Escritorio/TrabajoFinal/TrabajoFinalJuegoDeLaVida/src/main/java/com/example/NewInterfaz/muertos (2).png");
+        Image imageMuertos = new Image("file:///C:/Users/Raquel/OneDrive%20-%20Universidad%20de%20Alcala/Escritorio/TrabajoFinal/TrabajoFinalJuegoDeLaVida/src/main/java/com/example/NewInterfaz/Images/muertos (2).png");
         imageViewMuertos.setImage(imageMuertos);
-        Image imageFondo = new Image("file:///C:/Users/Raquel/OneDrive%20-%20Universidad%20de%20Alcala/Escritorio/TrabajoFinal/TrabajoFinalJuegoDeLaVida/src/main/java/com/example/NewInterfaz/fondo.png");
+        Image imageFondo = new Image("file:///C:/Users/Raquel/OneDrive%20-%20Universidad%20de%20Alcala/Escritorio/TrabajoFinal/TrabajoFinalJuegoDeLaVida/src/main/java/com/example/NewInterfaz/Images/fondo.png");
         imageViewFondo.setImage(imageFondo);
+        Image buttonVelocidad = new Image("file:///C:/Users/Raquel/OneDrive%20-%20Universidad%20de%20Alcala/Escritorio/TrabajoFinal/TrabajoFinalJuegoDeLaVida/src/main/java/com/example/NewInterfaz/Images/Velocidad.png");
+        buttonViewVelocidad.setImage(buttonVelocidad);
+        Image buttonPlay = new Image("file:///C:/Users/Raquel/OneDrive%20-%20Universidad%20de%20Alcala/Escritorio/TrabajoFinal/TrabajoFinalJuegoDeLaVida/src/main/java/com/example/NewInterfaz/Images/Play.png");
+        buttonViewPlay.setImage(buttonPlay);
+        Image buttonPause = new Image("file:///C:/Users/Raquel/OneDrive%20-%20Universidad%20de%20Alcala/Escritorio/TrabajoFinal/TrabajoFinalJuegoDeLaVida/src/main/java/com/example/NewInterfaz/Images/Pausa.png");
+        buttonViewPause.setImage(buttonPause);
+        Image buttonStop = new Image("file:///C:/Users/Raquel/OneDrive%20-%20Universidad%20de%20Alcala/Escritorio/TrabajoFinal/TrabajoFinalJuegoDeLaVida/src/main/java/com/example/NewInterfaz/Images/Stop.png");
+        buttonviewStop.setImage(buttonStop);
+        Image buttonSound = new Image("file:///C:/Users/Raquel/OneDrive%20-%20Universidad%20de%20Alcala/Escritorio/TrabajoFinal/TrabajoFinalJuegoDeLaVida/src/main/java/com/example/NewInterfaz/Images/VolumenON.png");
+        buttonViewSonido.setImage(buttonSound);
+        Image cuadroTexto = new Image("file:///C:/Users/Raquel/OneDrive%20-%20Universidad%20de%20Alcala/Escritorio/TrabajoFinal/TrabajoFinalJuegoDeLaVida/src/main/java/com/example/NewInterfaz/Images/Cuadro texto.png");
+        imageViewCuadroTexto.setImage(cuadroTexto);
+
     }
 
 }
