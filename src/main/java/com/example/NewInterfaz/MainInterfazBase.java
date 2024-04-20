ef0d3b9fd9119e4b72938a9bbc5f2566b22b6e75
raquelvegas <raquel.vegas@edu.uah.es>
2024-04-20 20:27:14 +0200

    20-04 20.27

--- src/main/java/com/example/NewInterfaz/MainInterfazBase.java
+++ src/main/java/com/example/NewInterfaz/MainInterfazBase.java
@@ -4,22 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 
 public class MainInterfazBase extends Application {
-    /*@Override
-    public void start(Stage primaryStage) throws IOException {
-        URL fxmlUrl = getClass().getResource("MenuInicial.fxml");
-        Parent root = FXMLLoader.load(fxmlUrl);
-        primaryStage.setTitle("¡Bienvenido!");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.setFullScreen(false);
-        primaryStage.setFullScreenExitHint("");
-        primaryStage.show();
-    }*/
 
     @Override
     public void start(Stage primaryStage) throws IOException {
@@ -34,7 +27,27 @@ public class MainInterfazBase extends Application {
         primaryStage.setMaximized(true); // Pantalla completa
         primaryStage.setResizable(false); // Evitar que la ventana sea redimensionable
         primaryStage.show();
-    }
+
+
+        primaryStage.setOnCloseRequest(event -> {
+            event.consume(); // Consumir el evento para evitar que la ventana se cierre directamente
+
+            // Mostrar un diálogo de confirmación
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmar salida");
+            alert.setHeaderText("¿Estás seguro de que quieres salir?");
+            alert.setContentText("Si sales, perderás todos los cambios no guardados.");
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                // Si el usuario confirma, cierra la aplicación
+                primaryStage.close();
+            }
+    });
+
+        primaryStage.show();
+}
+
 
     public static void main(String[] args) {
         launch();
